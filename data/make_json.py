e84154832375bfafab0a17957c7b0a1f56963766
Malaika Handa <mhanda@andrew.cmu.edu>
2019-03-04 17:06:24 -0500

    images, json

--- data/make_json.py
+++ data/make_json.py
@@ -1,18 +1,26 @@
+################################################################################
+# SET UP
+################################################################################
+
 # imports
 from collections import Counter
 import itertools
 import json
+import os
 
 # read in the file
-with open("data.csv") as f:
+with open("data_big.csv") as f:
     data = f.read()
     rows = data.split("\n")
 
-# goal:
-# list of nodes, each with a name
-# list of edges, each with a source, target, and value
+# read in the images
+images = os.listdir("../images")
 
-# node info
+################################################################################
+# GRAPH
+################################################################################
+
+# nodes
 
 all_items = []
 for row in rows:
@@ -24,19 +32,23 @@ counts = Counter(all_items)
 node_dict = {}
 nodes = []
 for i, n in enumerate(counts):
+    image_name = n.replace(" ", "_") + ".png"
+    if (image_name not in images):
+        continue
     node = {
         "name": n,
         "value": counts[n],
-        "img": "https://raw.githubusercontent.com/malaikahanda/clothes/master/images/{name}.png".format(name=n.replace(" ", "_"))
+        "img": "https://raw.githubusercontent.com/malaikahanda/clothes/master/images/{}".format(image_name)
     }
     nodes.append(node)
     node_dict[n] = i
 
-# edge info
+# edges
 
 all_pairs = []
 for row in rows:
     items = sorted([i for i in row.split(",")[1 :] if (i != "")])
+    items = [i for i in items if i in node_dict]
     pairs = itertools.combinations(items, 2)
     all_pairs += pairs
 
@@ -51,10 +63,30 @@ for (src, tgt) in counts:
     }
     edges.append(edge)
 
-# write output
+################################################################################
+# DATES
+################################################################################
+
+# dates = {}
+# for row in rows:
+#     try:
+#         items = [i for i in row.split(",") if (i != "")]
+#         date = items[0]
+#         clothes = items[1 :]
+#         dates[date] = clothes
+#     except:
+#         continue
+
+################################################################################
+# OUTPUT
+################################################################################
+
 graph = {
     "nodes": nodes,
     "links": edges
 }
 with open("graph.json", "w") as f:
     json.dump(graph, f)
+
+# with open("dates.json", "w") as f:
+#     json.dump(dates, f)
