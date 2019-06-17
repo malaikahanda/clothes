eadaa45c3fd2c6d9d7a7c5a43798c89379e429b7
Malaika Handa <mhanda@andrew.cmu.edu>
2019-06-17 16:45:25 -0400

    restructure json

---
+++ data/format_nodes_data.py
@@ -0,0 +1,77 @@
+################################################################################
+# SET UP
+################################################################################
+
+# imports
+from collections import Counter
+import itertools
+import json
+import numpy as np
+import os
+import pandas as pd
+import random
+
+# read in the files
+nodes = pd.read_csv("node_attributes.csv").set_index("item")
+days = pd.read_csv("days.csv").fillna("")
+images = os.listdir("../images")
+
+# globals
+COL = "color"
+R_MIN = 8
+R_MAX = 30
+
+
+################################################################################
+# CREATE THE NODES
+################################################################################
+
+# get counts
+items = []
+for i, row in days.iterrows():
+    not_null = [e for e in row.tolist()[1:] if e != ""]
+    items.extend(not_null)
+counts = Counter(items)
+
+# print("NEED PICTURES:")
+for item in counts.keys():
+    image_name = item.replace(" ", "_") + ".png"
+    if (image_name not in images):
+        print(item)
+
+# print("")
+# print("COUNTS:")
+for e in counts.most_common(len(counts)):
+    # print(e)
+    continue
+
+# add count column
+data = {"item": list(counts.keys()), "n_worn": list(counts.values())}
+count_df = pd.DataFrame(data).set_index("item")
+nodes = count_df.join(nodes)
+
+# scale the counts to be reasonable
+old_min = min(nodes["n_worn"])
+old_max = max(nodes["n_worn"])
+nodes["radius"] = (((nodes["n_worn"] - old_min) * (R_MAX - R_MIN)) / (old_max - old_min)) + R_MIN
+nodes = nodes.round({"radius": 0})
+
+# map the sorting column to an int
+nodes[COL] = nodes[COL].astype("category")
+nodes["sorter"] = nodes[COL].cat.codes
+
+# add image column
+IMG_STRING = "https://raw.githubusercontent.com/malaikahanda/clothes/master/images/{}.png"
+nodes["img"] = [IMG_STRING.format(node.replace(" ", "_")) for node in nodes.index.tolist()]
+
+
+################################################################################
+# OUTPUT
+################################################################################
+
+# create the graph
+n = nodes.to_dict("index")
+
+# save it
+with open("nodes.json", "w") as f:
+    json.dump(n, f)
--- scroll/smol.js
+++ scroll/smol.js
@@ -1,119 +1,50 @@
-// read in a graph
-var url = "https://raw.githubusercontent.com/malaikahanda/clothes/master/data/graph.json";
-d3.json(url).then(function(data) {
-  console.log(data);
+var width = 600, height = 400;
+
+var colorScale = ['orange', 'lightblue', '#B19CD9']; // unnecessary
+var xCenter = [100, 300, 500] // unnecessary
+
+// read in from json
+var numNodes = 100;
+var nodes = d3.range(numNodes).map(function(d, i) {
+  return {
+    radius: Math.random() * 25,
+    category: i % 3
+  }
 });
 
-/*.then(function(data) {
-  console.log(data);
-});*/
+console.log(nodes);
 
+var simulation = d3.forceSimulation(nodes)
+  .force('charge', d3.forceManyBody().strength(5))
+  .force('x', d3.forceX().x(function(d) {
+    return xCenter[d.category]; // this can be d.xCenter?
+  }))
+  .force('collision', d3.forceCollide().radius(function(d) {
+    return d.radius;
+  }))
+  .on('tick', ticked);
 
-// d3.json(url, function(error, graph) {
+function ticked() {
+  var u = d3.select('svg g')
+    .selectAll('circle')
+    .data(nodes);
 
-//     // console.log(graph);
+  u.enter()
+    .append('circle')
+    .attr('r', function(d) {
+      return d.radius;
+    })
+    .style('fill', function(d) {
+      return colorScale[d.category]; // this can be d.color?
+    })
+    .merge(u)
+    .attr('cx', function(d) {
+      return d.x;
+    })
+    .attr('cy', function(d) {
+      return d.y;
+    })
 
-//     var width = 640;
-//     var height = 720;
-//     console.log("finished");
+  u.exit().remove();
 
-// });
-
-    /*console.log(width);
-
-    // segment the json
-    var nodes = graph.nodes;
-    console.log("nodes");
-    console.log(nodes);
-    var links = graph.links;
-    console.log(links);
-    var root = nodes[0];
-
-    root.radius = 0;
-    root.fixed = true;
-
-    // create a force layout objects using our graph
-    var force = d3.layout.force()
-        //.gravity(0.05)
-        //.charge(function(d, i) { return i ? 0 : -2000; })
-        .nodes(nodes)
-        .links(links)
-        .size([width, height]);
-
-    force.start()
-
-    var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    var node = svg.selectAll(".node")
-        .data(nodes)
-        .enter()
-        .append("g")
-        .attr("class", "node")
-        .append("image")
-        .attr("xlink:href",  function(d) { return d.img; })
-        .attr("x", function(d) { return -25; })
-        .attr("y", function(d) { return -25; })
-        .attr("height", 40)
-        .attr("width", 40);
-        // .attr("height", function(d) { return d.radius * 2; })
-        // .attr("width", function(d) { return d.radius * 2; });
-
-    // do this every millisecond (or arbitrary time length)
-    force.on("tick", function(e) {
-
-        var q = d3.geom.quadtree(nodes);
-        var i = 0;
-        var n = nodes.length;
-
-        while (++i < n) q.visit(collide(nodes[i]));
-
-        svg.selectAll("image")
-            .attr("x", function(d) { return d.x; })
-            .attr("y", function(d) { return d.y; });
-    });
-
-
-    // svg.on("mousemove", function() {
-    //     var p1 = d3.mouse(this);
-    //     root.px = p1[0];
-    //     root.py = p1[1];
-    //     force.resume();
-    // });
-
-
-    function collide(node) {
-
-        // var r = node.radius + 16;
-        var r = 40 + 16;
-        var nx1 = node.x - r;
-        var nx2 = node.x + r;
-        var ny1 = node.y - r;
-        var ny2 = node.y + r;
-
-        return function(quad, x1, y1, x2, y2) {
-
-            if (quad.point && (quad.point !== node)) {
-                var x = node.x - quad.point.x;
-                var y = node.y - quad.point.y;
-                var l = Math.sqrt(x * x + y * y);
-                // var r = node.radius + quad.point.radius;
-                var r = 40 + quad.point.radius;
-
-                if (l < r) {
-                    l = (l - r) / l * .5;
-                    node.x -= x *= l;
-                    node.y -= y *= l;
-                    quad.point.x += x;
-                    quad.point.y += y;
-                }
-
-            }
-
-        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-        };
-
-    }
-
-});*/
+}
