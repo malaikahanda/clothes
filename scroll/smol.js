acd044fadbd0fd7f5465eaf4f48a371f3ee1600d
Malaika Handa <mhanda@andrew.cmu.edu>
2019-09-29 23:41:35 -0400

    1. radio button is technically functional, 2. all images are broken, 3. no idea why

--- scroll/smol.js
+++ scroll/smol.js
@@ -1,8 +1,22 @@
+function displayRadioValue() { 
+  var ele = document.getElementsByName("sorter"); 
+  var selected = null;
+  for(i = 0; i < ele.length; i++) { 
+    if(ele[i].checked) {
+      selected = ele[i].value;
+      document.getElementById("selectedSorter").innerHTML
+        = "Result: "+ selected;
+    }
+  }
+  return selected;
+} 
+
 var url = "https://raw.githubusercontent.com/malaikahanda/clothes/master/data/nodes.json";
 d3.json(url).then(function(nodes) {
 
   // sortby
   var sorter = "color";
+  /// sorter = displayRadioValue(); // TODO: change this eventually to be an input
   var allSorters = nodes.map(d => d[sorter]);
   var uniqueSorters = Array.from(new Set(allSorters));
   var numSorters = uniqueSorters.length;
@@ -14,7 +28,8 @@ d3.json(url).then(function(nodes) {
 
 
   var width = 1280, height = 720;
-  // this needs to be better lol
+
+  // TODO: this needs to be better lol-- should adjust dynamically based on screen size
   var xCenter = [100, 300, 500, 700, 100, 300, 500, 700, 100, 300, 500, 700];
   var yCenter = [-200, -200, -200, -200, 0, 0, 0, 0, 200, 200, 200, 200];
 
@@ -22,7 +37,9 @@ d3.json(url).then(function(nodes) {
     .force('charge', d3.forceManyBody().strength(5))
     .force('x', d3.forceX().x(function(d) { return xCenter[d.sorter]; }))
     .force('y', d3.forceY().y(function(d) { return yCenter[d.sorter]; }))
+    // TODO: if the radius is fixed, this should be dynamic as well, based on screen size
     .force('collision', d3.forceCollide().radius(20))
+    // still unsure... do i want to size nodes? or keep them constant
     // .force('collision', d3.forceCollide().radius(function(d) { return d.radius; }))
     .on('tick', ticked);
 
